94e480854551d720c0fb9115a953a918ad1f173d
TimE1211 <timmy121192@gmail.com>
2017-05-24 16:23:38 -0400

    added label in code

--- app/ViewController.swift
+++ app/ViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class ViewController: UIViewController
 {
 
-  @IBOutlet weak var label: UILabel!
+  @IBOutlet weak var label1: UILabel!
   override func viewDidLoad()
   {
     super.viewDidLoad()
-    label.text = "For you Khaleel.... XD Khalil!"
+    label1.text = "For you Khaleel.... XD Khalil!"
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+    label.center = CGPoint(x: 160, y: 285)
+    label.textAlignment = .center
+    label.text = "I'am a test label"
+    self.view.addSubview(label)
   }
 
   override func didReceiveMemoryWarning()
