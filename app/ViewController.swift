a44727622916e467ea5eb9ff6e161a2db2047cdb
TimE1211 <timmy121192@gmail.com>
2017-05-24 16:15:13 -0400

    initial commit and added Label with text

---
+++ app/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Timothy Hang on 5/24/17.
+//  Copyright © 2017 Timothy Hang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+  @IBOutlet weak var label: UILabel!
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+    label.text = "For you Khaleel.... XD Khalil!"
+  }
+
+  override func didReceiveMemoryWarning()
+  {
+    super.didReceiveMemoryWarning()
+  }
+
+  
+}
+
